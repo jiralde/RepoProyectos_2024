80c0b83e2fe10ce27ff5caced95068f4cd9967d7
giovanni-peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2023-12-09 15:08:40 -0300

    +proyecto

---
+++ Minesperwere python/mineserwere.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+ROJO = (255, 0, 0) 
+VERDE = (0, 255, 0) 
+AZUL = (0, 0, 255)
+
+pygame.init()#inisialisa pygame
+
+ventana = pygame.display.set_mode((600, 400))#crea una ventana
+
+pygame.display.set_caption('minesspearwere')
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
