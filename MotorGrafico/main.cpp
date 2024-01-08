84f7859a1b6838abfcba4959b7617e09294f0446
giovanni-peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-01-08 14:06:37 -0300

    :)

--- MotorGrafico/main.cpp
+++ MotorGrafico/main.cpp
@@ -5,9 +5,36 @@
 #include<iostream>
 using namespace std;
 
+class cordenada{
+	private:
+		int x,y,z;
+	public:
+		cordenada();
+		void setPunto(int,int);
+		int getPuntoX();
+		int getPuntoY();
+		int getPuntoZ();
+}
+
+void cordenada::setPunto(int _x, int _y, int _z){
+	x = _x;
+	z = _z;
+	y = _y;
+}
+
+int cordenada::getPuntoX(){
+	return x;
+}
+int cordenada::getPuntoZ(){
+	return z;
+}
+int cordenada::getPuntoY(){
+	return y;
+}
 
 int main(){
-	cout << "hola Mundo";
+	
+	cout << "hola Mundo" << "\n";
 }
 
 
